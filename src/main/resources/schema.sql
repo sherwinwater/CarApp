c1fc2b864ca5b2545b6c70d0da03fd1dcbd26db1
sherwin <sherwin.water@gmail.com>
2020-11-01 21:23:57 -0500

    heroku

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,2 +1,3 @@
 DROP TABLE if exists cars;
-CREATE TABLE cars (id INT NOT NULL Primary Key AUTO_INCREMENT, make VARCHAR(16), model VARCHAR(16), colour VARCHAR(10),price DEC(10,2),VIN VARCHAR(20),dealership VARCHAR(25));
+CREATE TABLE cars (id SERIAL NOT NULL Primary Key, make VARCHAR(16), model VARCHAR(16), colour VARCHAR(10),price DEC(10,2),VIN VARCHAR(20),dealership VARCHAR(25));
+--CREATE TABLE cars (id INT NOT NULL Primary Key AUTO_INCREMENT, make VARCHAR(16), model VARCHAR(16), colour VARCHAR(10),price DEC(10,2),VIN VARCHAR(20),dealership VARCHAR(25));
