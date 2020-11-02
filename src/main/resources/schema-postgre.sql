8cf879cc2c9906d5171ea4eb11c463091fe7f6d9
sherwin <sherwin.water@gmail.com>
2020-11-01 21:28:56 -0500

    heroku

--- src/main/resources/schema-postgre.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE if exists cars;
-CREATE TABLE cars (id SERIAL NOT NULL Primary Key, make VARCHAR(16), model VARCHAR(16), colour VARCHAR(10),price DEC(10,2),VIN VARCHAR(20),dealership VARCHAR(25));
